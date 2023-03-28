c2e37dac68c842191cb197d7cb3504a7b9d5fd6b
khan3575 <sakibkhan.stud@gmail.com>
2023-03-29 01:09:15 +0600

    twopointer cf

---
+++ BUBT Prac/twopointer.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    ll n,s;
+    cin>>n>>s;
+    ll a[n];
+    for(int i=0; i<n;i++)
+        cin>>a[i];
+    ll x=0,res=0,l=0,r;
+    for(r=0; r<n; r++)
+    {
+        x+=a[r];
+        while(x-a[l]>=s)
+        {
+            x-=a[l];
+            l++;
+        }
+        if(x>=s)
+        {
+            res+= n-(r-l+1);
+            //cout<<res<< " ";
+        }
+    }
+    cout<<res<<endl;
+}
