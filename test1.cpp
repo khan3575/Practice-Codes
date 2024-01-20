4a6cac8a6ad9d0092e012137fc3d9c9f6a831d86
sakibkhan <sakibkhan.stud@gmail.com>
2024-01-20 16:07:51 +0600

    test cpp added

---
+++ test1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		cout<<t<<endl;
+	}
+	return 0;
+}
