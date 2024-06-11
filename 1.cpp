176f7f307870d9ae03bb8e90e3b1994137ee9e38
sakibkhan <sakibkhan.stud@gmail.com>
2024-06-11 07:23:42 +0600

    solved Get the container Problem

--- 1.cpp
+++ 1.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 int main()
 {
-	int t;
-	cin>>t;
-	cout<< t*6;
+	string s;
+	getline(cin,s);
+	int cnt=0;
+	for(int i=0; i<s.size(); i++)
+	{
+		if(tolower(s[i])=='a'||tolower(s[i])=='e'||tolower(s[i])=='i'||tolower(s[i])=='o'||tolower(s[i])=='u' )
+		{
+			cnt++;
+		}
+	}
+	cout<<cnt<<endl;
+
 }
